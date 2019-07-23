478db11f3286628287908bb286f70150c186b7d4
Snowert <andy@chabalier.com>
2019-07-23 11:35:08 +0200

    Addition of preview field for forum creation

--- app/src/main/java/com/alten/ambroise/forum/view/fragments/ForumAddDialogFragment.java
+++ app/src/main/java/com/alten/ambroise/forum/view/fragments/ForumAddDialogFragment.java
@@ -3,12 +3,16 @@ package com.alten.ambroise.forum.view.fragments;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.Gravity;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.DialogFragment;
 
@@ -22,6 +26,7 @@ public class ForumAddDialogFragment extends DialogFragment {
     private EditText inputName;
     private EditText inputPlace;
     private DatePicker inputDate;
+    private TextView previewTextView;
 
     // Empty constructor required for DialogFragment
     public ForumAddDialogFragment() {
@@ -44,6 +49,20 @@ public class ForumAddDialogFragment extends DialogFragment {
         String title = getArguments().getString("title");
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
         alertDialogBuilder.setTitle(title);
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                updateTextPreview();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        };
 
         LinearLayout layout = new LinearLayout(this.getContext());
         layout.setLayoutParams(new LinearLayout.LayoutParams(
@@ -55,15 +74,33 @@ public class ForumAddDialogFragment extends DialogFragment {
         inputName = new EditText(this.getContext());
         inputName.setHint(R.string.forum_name);
         inputName.setInputType(InputType.TYPE_CLASS_TEXT);
+        inputName.addTextChangedListener(textWatcher);
         layout.addView(inputName);
 
         inputPlace = new EditText(this.getContext());
         inputPlace.setHint(R.string.forum_place);
         inputPlace.setInputType(InputType.TYPE_CLASS_TEXT);
+        inputPlace.addTextChangedListener(textWatcher);
         layout.addView(inputPlace);
 
         inputDate = new DatePicker(this.getContext());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            inputDate.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
+                @Override
+                public void onDateChanged(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                    updateTextPreview();
+                }
+            });
+        }
         layout.addView(inputDate);
+
+        TextView preview = new TextView(this.getContext());
+        preview.setText(R.string.preview);
+        layout.addView(preview);
+
+        previewTextView = new TextView(this.getContext());
+        layout.addView(previewTextView);
+        updateTextPreview();
 
         alertDialogBuilder.setView(layout);
 
@@ -71,10 +108,10 @@ public class ForumAddDialogFragment extends DialogFragment {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 Forum newForum = new Forum();
-                if(inputName.length() == 0){
+                if (inputName.length() == 0) {
                     inputName.setText(R.string.unknown);
                 }
-                if(inputPlace.length() == 0){
+                if (inputPlace.length() == 0) {
                     inputPlace.setText(R.string.unknown);
                 }
                 newForum.setName(inputName.getText().toString());
@@ -96,6 +133,21 @@ public class ForumAddDialogFragment extends DialogFragment {
         });
 
         return alertDialogBuilder.create();
+    }
+
+    private void updateTextPreview() {
+        // Check if day and month have only one digit. If it's the case, then add 0 before the digit to match with xx format
+        String day = inputDate.getDayOfMonth() <= 9 ? "0" + inputDate.getDayOfMonth() : String.valueOf(inputDate.getDayOfMonth());
+        String month = inputDate.getMonth() <= 9 ? "0" + inputDate.getMonth() : String.valueOf(inputDate.getMonth());
+        String name = inputName.getText().length() == 0 ? getResources().getString(R.string.unknown) : inputName.getText().toString();
+        String place = inputPlace.getText().length() == 0 ? getResources().getString(R.string.unknown) : inputPlace.getText().toString();
+        String text = new StringBuilder().append(name)
+                .append(" - ").append(day)
+                .append(" ").append(month)
+                .append(" ").append(inputDate.getYear())
+                .append(" – ").append(place)
+                .toString();
+        previewTextView.setText(text);
     }
 
 }
