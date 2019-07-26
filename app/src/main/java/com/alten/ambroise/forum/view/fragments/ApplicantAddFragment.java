cac3f508de209d1b0ebca141c6492f87e6741a27
Snowert <andy@chabalier.com>
2019-07-26 11:06:20 +0200

    Allow taking photo and display it in hight resolution

--- app/src/main/java/com/alten/ambroise/forum/view/fragments/ApplicantAddFragment.java
+++ app/src/main/java/com/alten/ambroise/forum/view/fragments/ApplicantAddFragment.java
@@ -1,17 +1,33 @@
 package com.alten.ambroise.forum.view.fragments;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
 import com.alten.ambroise.forum.R;
 import com.alten.ambroise.forum.data.model.beans.ApplicantForum;
 import com.alten.ambroise.forum.view.fragmentSwitcher.ApplicantFragmentSwitcher;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION;
+import static android.content.Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +42,10 @@ public class ApplicantAddFragment extends Fragment {
     private OnFragmentInteractionListener mListener;
 
     private ApplicantFragmentSwitcher applicantFragmentSwitcher;
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private String currentPhotoPath;
+
 
     public ApplicantAddFragment() {
         // Required empty public constructor
@@ -56,13 +76,81 @@ public class ApplicantAddFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_applicant_add, container, false);
+        View view = inflater.inflate(R.layout.fragment_applicant_add, container, false);
+        //Add take picture action
+        view.findViewById(R.id.cv_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+        //add preview picture action
+        view.findViewById(R.id.cv_display).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri =  FileProvider.getUriForFile(getActivity(),
+                        "com.alten.ambroise.forum.fileprovider",
+                        new File(currentPhotoPath));
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_QUICK_VIEW);
+                intent.setDataAndType(uri, "image/*");
+                intent.setFlags(FLAG_GRANT_READ_URI_PERMISSION | FLAG_GRANT_WRITE_URI_PERMISSION);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(ApplicantForum applicant) {
         if (mListener != null) {
             mListener.onFragmentInteraction(applicant);
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  //prefix
+                ".jpg",         //suffix
+                storageDir      //directory
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Toast.makeText(this.getContext(),getString(R.string.error_creating_file),Toast.LENGTH_SHORT);
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(getActivity(),
+                        "com.alten.ambroise.forum.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            ((ImageView) getActivity().findViewById(R.id.cv_display)).setImageURI(Uri.fromFile(new File(currentPhotoPath)));
         }
     }
 
