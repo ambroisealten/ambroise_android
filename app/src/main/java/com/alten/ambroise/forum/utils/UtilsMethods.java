4a8cd55804d9c65e16d58bc7543eb15e639f33a6
Snowert <andy@chabalier.com>
2019-08-14 11:25:31 +0200

    Control on multiple mail in gradeAndSend Fragment. Allow control on list of mail separated with comma

--- app/src/main/java/com/alten/ambroise/forum/utils/UtilsMethods.java
+++ app/src/main/java/com/alten/ambroise/forum/utils/UtilsMethods.java
@@ -12,4 +12,14 @@ public class UtilsMethods {
     public final static boolean isValidPhoneNumber(CharSequence target) {
         return !TextUtils.isEmpty(target) && Patterns.PHONE.matcher(target).matches();
     }
+
+    public static boolean isValidMultipleEmail(CharSequence targets, final String separator) {
+        final String[] targetSpliced = targets.toString().split(separator);
+        for (CharSequence target : targetSpliced) {
+            if (!isValidEmail(target)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
