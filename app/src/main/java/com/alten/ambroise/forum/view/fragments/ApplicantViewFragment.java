72865f470f43daae6ca948ebbc4e478fadf7c163
unknown <camille.schnell@alten.com>
2019-08-19 15:07:52 +0200

    add cv display

--- app/src/main/java/com/alten/ambroise/forum/view/fragments/ApplicantViewFragment.java
+++ app/src/main/java/com/alten/ambroise/forum/view/fragments/ApplicantViewFragment.java
@@ -1,20 +1,24 @@
 package com.alten.ambroise.forum.view.fragments;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.room.util.StringUtil;
-
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridView;
-import android.widget.ListView;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 import com.alten.ambroise.forum.R;
 import com.alten.ambroise.forum.data.model.Mobility;
@@ -27,13 +31,15 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.google.gson.internal.LinkedTreeMap;
-import com.google.gson.reflect.TypeToken;
 
-import java.lang.reflect.Type;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.stream.Collectors;
+
+import static android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION;
+import static android.content.Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
+import static com.alten.ambroise.forum.view.fragments.ApplicantAddFragment.REQUEST_IMAGE_CAPTURE;
 
 
 public class ApplicantViewFragment extends Fragment {
@@ -42,6 +48,7 @@ public class ApplicantViewFragment extends Fragment {
     private FragmentSwitcher switcher;
     private GridView skillsGridView;
     private GridView mobilityGridView;
+    private ImageView cvDisplay;
 
     private ApplicantForum applicant;
     private long applicantId = 0;
@@ -86,13 +93,39 @@ public class ApplicantViewFragment extends Fragment {
         ((TextView)view.findViewById(R.id.applicant_view_personInChargeMail)).setText(this.applicant.getPersonInChargeMail());
         ((TextView)view.findViewById(R.id.applicant_view_driverLicense)).setText(this.applicant.isDriverLicense() ? "Driver license" : "");
         ((TextView)view.findViewById(R.id.applicant_view_vehicule)).setText(this.applicant.isVehicule() ? "Own vehicule" : "");
-        ((TextView)view.findViewById(R.id.applicant_view_nationality)).setText(this.applicant.getNationality() == null ? "" : this.applicant.getNationality().toString());
+        ((TextView)view.findViewById(R.id.applicant_view_nationality)).setText(this.applicant.getNationality() == null ? "" : "Nationality : " + this.applicant.getNationality().toString());
         // Skills list
         this.skillsGridView = view.findViewById(R.id.applicant_view_skills_grid_view);
         refreshSkillsGridView();
         // Mobility list
         this.mobilityGridView = view.findViewById(R.id.applicant_view_mobility_grid_view);
         refreshMobilityGridView();
+        // Share button
+        Button shareApplicant = view.findViewById(R.id.applicant_view_share);
+        shareApplicant.setOnClickListener(v -> {
+
+           // mListener.onFragmentInteraction(true, RGPDFragmentSwitcher.RGPD_GRADE_AND_SEND_TAG, applicant);
+        });
+        // CV display
+        cvDisplay = view.findViewById(R.id.applicant_view_cv_display);
+        byte[] decodedString = Base64.decode(this.applicant.getCvPerson(), Base64.DEFAULT);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        cvDisplay.setImageBitmap(decodedByte);
+        //add preview picture action
+        cvDisplay.setOnClickListener(v -> {
+            if (cvDisplay.getDrawable() != null) {
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                decodedByte.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+                String path = MediaStore.Images.Media.insertImage(getContext().getContentResolver(), decodedByte, "Title", null);
+                Uri uri = Uri.parse(path);
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_QUICK_VIEW);
+                intent.setDataAndType(uri, "image/*");
+                intent.setFlags(FLAG_GRANT_READ_URI_PERMISSION | FLAG_GRANT_WRITE_URI_PERMISSION);
+                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+            }
+        });
+
         return view;
     }
 
